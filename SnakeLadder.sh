9bc3725585720ca08d7b8253886304b9756d7725
Rutujakalyane <60542373+Rutujakalyane@users.noreply.github.com>
2020-04-07 17:20:05 +0530

    ADDED : Option to moving position of player

--- SnakeLadder.sh
+++ SnakeLadder.sh
@@ -1,8 +1,33 @@
 echo "Welcome to Snake and Ladder simulator"
+
+INITIAL_POSITION=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+
 player=1
-position=0
-function roll(){
-	num=$(( RANDOM % 6 + 1))
+playerPosition=$INITIAL_POSITION
+
+function game(){
+
+        dieNum=$(( RANDOM % 6 + 1))
+        option=$(( RANDOM % 3 ))
+
+        case $option in
+        $NO_PLAY)
+                playerPosition=$(( playerPosition + 0 ))
+                echo "No Play you can stays in this"
+                ;;
+        $SNAKE)
+                playerPosition=$(( playerPosition - dieNum ))
+                echo "Snake-player moves behind by $dieNum received on the die"
+                ;;
+        $LADDER)
+                playerPosition=$(( playerPosition + dieNum ))
+                echo "Ladder-player moves ahead by $dieNum received on the die"
+                ;;
+        esac
 }
-roll
-echo "Die number - $num"
+game
+echo "Player Position - $playerPosition"
+
